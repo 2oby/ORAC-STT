33d36abed063047e09e10749141573eb9ced6f65
2oby <toby.weston@mac.com>
2025-08-29 16:31:13 +0200

    Speed up heartbeat animation to 1 second

--- src/orac_stt/web/static/js/admin.js
+++ src/orac_stt/web/static/js/admin.js
@@ -754,11 +754,11 @@ class OracSTTAdmin {
                 
                 if (dataChanged && !existingCard.classList.contains('refreshing')) {
                     existingCard.classList.add('refreshing');
-                    // Remove class after animation completes (2s duration)
+                    // Remove class after animation completes (1s duration)
                     existingCard.dataset.animationTimeout = setTimeout(() => {
                         existingCard.classList.remove('refreshing');
                         delete existingCard.dataset.animationTimeout;
-                    }, 2000);
+                    }, 1000);
                 }
                 
                 // Ensure card is in correct position
